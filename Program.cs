768c92cf82aba6462dbc3429d0eb52fd12136fb6
Stanislav <stas2004pet@gmail.com>
2019-12-14 17:20:56 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassStudentLibrary;
+
+namespace Programm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StudentService service = new StudentService();
+            service.Add(new Student
+            {
+                Name = "Karl",
+                Age = 54,
+                LastName = "Marx"
+            });
+            Random rand = new Random();
+            foreach (Student item in service.Students)
+            {
+                item.AddMark("Biology", rand.Next(1, 12));
+                item.AddMark("Physic", rand.Next(1, 12));
+            }
+            foreach (Student item in service.Students)
+            {
+                Console.WriteLine("Name : " + item.Name + "\nLast Name : " + item.LastName + "\nAge : " + item.Age + "\n");
+            }
+            
+        }
+    }
+}
